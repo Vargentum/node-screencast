f49b0a13cf1f7d8a8a5fb8ae4edfc81a8605d07e
Vargentum <vargentum@gmail.com>
2016-09-12 22:43:45 +0300

    14. simple http server

---
+++ webserver/server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var server = new http.Server()
+
+server.listen(9023, '127.0.0.1')
+
+var counter = 0
+server.on('request', function(req, res) {
+  res.end("Hello, world " + ++counter);
+})
